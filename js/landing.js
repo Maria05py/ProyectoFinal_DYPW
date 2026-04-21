e9f69df246ff706dbe5255b63a6b09d4d4e4d45c
Sebastián Salas <sebastiansalash19@icloud.com>
2026-04-20 20:33:19 -0600

    Agregando responsive, input del e-mail y corrección a la galeria de img

--- js/landing.js
+++ js/landing.js
@@ -8,3 +8,21 @@ itemsGaleria.forEach((item) => {
         item.classList.remove("activa");
     });
 });
+
+const email = document.getElementById("e-mail");
+const regexCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+email.addEventListener("blur", () => {
+
+    const mensaje = document.getElementById("mensaje");
+
+    if (email.value === "" || !regexCorreo.test(email.value)) {
+        mensaje.textContent = "Escribe un correo electrónico válido";
+        mensaje.style.display = "block";
+        mensaje.style.color = "#ff6b6b";
+    } else {
+        mensaje.textContent = "Se ha enviado el correo electrónico correctamente";
+        mensaje.style.display = "block";
+        mensaje.style.color = "#4CAF50";
+    }
+});
